4debcb4dba90a29bd8238af50878dbb389b69165
James Hawley <jamesrichardhawley@gmail.com>
2019-10-25 15:02:58 -0400

    Separating `gis` into its own module

--- genomis/_cli.py
+++ genomis/_cli.py
@@ -16,8 +16,9 @@ def validate(ARGS):
     if len(nonex_files) != 0:
         raise IOError(" ".join([nonex_files[0], "not found."]))
     # check `--names` and beds are of the same length
-    if ARGS.names is not None:
-        names = ARGS.names.split(",")
+    names = ARGS.names
+    if names is not None:
+        names = names.split(",")
         if len(names) != len(ARGS.bed):
             raise IOError(
                 "`--names` and `bed` must have the same number of arguments if `--names` is specified."
@@ -40,15 +41,11 @@ def main():
         help="Prefix for output files.",
         default="similarity",
     )
-
     # parse arguments from command line
     ARGS = PARSER.parse_args()
-
     # validate command line arguments
     # import packages after parsing to speed up command line responsiveness
     validated_args = validate(ARGS)
-    from .genomis import gis
+    from .gis import gis
 
-    val = gis(**validated_args)
-    if val is not None:
-        print(val)
+    gis(**validated_args)
