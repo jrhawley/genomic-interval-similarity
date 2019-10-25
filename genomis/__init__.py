5e181309f50177bf8301d9501a0fadf6214e482e
James Hawley <jamesrichardhawley@gmail.com>
2019-10-25 13:51:52 -0400

    Completed recording of similarity scores and saving to output
    * Added minimum similarity threshold to consider the set of intervals
    * Column-/row-wise updating of the similarity matrix for each new interval
    * Recording interval similarities when conditions for recording are met
      * Following the optimal format for appending records as detailed in https://stackoverflow.com/a/47979665/7416009
    * Saving to output TSV via pandas

--- genomis/__init__.py
+++ genomis/__init__.py
@@ -1,6 +1,7 @@
 import numpy as np
-from interval import interval, inf, imath
+from interval import interval
 from itertools import combinations
+import pandas as pd
 
 __version__ = "0.0.1"
 
@@ -11,6 +12,9 @@ class GenomicInterval(object):
     """
 
     def __init__(self, chrom, start, end):
+        """
+        Initialize object
+        """
         self.chr = chrom
         self.inf = int(start)
         self.sup = int(end)
@@ -18,9 +22,21 @@ class GenomicInterval(object):
         self.interval = interval([self.inf, self.sup])
 
     def __str__(self):
+        """
+        What to print when object is returned to interactive terminal
+        """
         return self.chr + ":" + str(self.inf) + "-" + str(self.sup)
 
+    def __repr__(self):
+        """
+        What to print when object is represented
+        """
+        return self.__str__()
+
     def __len__(self):
+        """
+        What to return when len() is called
+        """
         return self.size
 
 
@@ -49,13 +65,6 @@ class Bed:
         Close object and release from memory
         """
         self.fh.close()
-
-
-# class SymArray(np.array):
-#     def __setitem__(self, indexes, value):
-#         i, j = indexes
-#         super(SymArray, self).__setitem__((i, j), value)
-#         super(SymArray, self).__setitem__((j, i), value)
 
 
 def symmetrize(a):
@@ -87,7 +96,7 @@ def similarity(a, b):
         return 0
 
 
-def gis(bedfiles, names=None, prefix="similarity"):
+def gis(bedfiles, names=None, prefix="similarity", sim_thresh=0.5):
     """
     Calculate genomic similarity of BED files
 
@@ -99,11 +108,18 @@ def gis(bedfiles, names=None, prefix="similarity"):
         Name for each of the input BED files
     prefix : str
         Output file prefix
+    sim_thresh : float
+        Minimum similarity threshold to consider recording a locus
     """
     # file handles for each BED file
     n = len(bedfiles)
+    # store sample indexes as `names` is `names` is not defined
+    if names is None:
+        names = ["Sample_" + str(i) for i in range(n)]
     # store similarity matrix
     sim_mat = np.identity(n)
+    # columns of information to store
+    column_names = ["chr", "start", "end"] + names + ["similarity"]
     # initialize `bed` objects
     beds = [Bed(b) for b in bedfiles]
     # get first intervals from each file
@@ -116,7 +132,57 @@ def gis(bedfiles, names=None, prefix="similarity"):
     minsim = [
         {"idx": j, "s": sim_mat[i, j]} for i, j in enumerate(np.argmin(sim_mat, axis=1))
     ]
-    print(sim_mat)
-    print(minsim)
-    return
-
+    # records to keep for printing
+    records = []
+    # iterate over intervals from the sorted BED files
+    while True:
+        minsim_set = min([ms["s"] for ms in minsim])
+        # record this set if the similarity of the set passes the threshold
+        if minsim_set >= sim_thresh:
+            chrom = intvls[0].chr
+            # skip if not all intervals are on the same chromosome
+            if np.all([intvl.chr == chrom for intvl in intvls]):
+                # find interval that spans entire set of intervals
+                hull = interval.hull([intvl.interval for intvl in intvls])
+                set_locus = GenomicInterval(chrom, hull[0].inf, hull[0].sup)
+                records.append(
+                    dict(
+                        (colname, v)
+                        for colname, v in zip(
+                            column_names,
+                            [
+                                set_locus.chr,
+                                set_locus.inf,
+                                set_locus.sup,
+                                *[b.counter for b in beds],
+                                minsim_set,
+                            ],
+                        )
+                    )
+                )
+        # find sample with the smallest upper bound
+        update_idx = np.argmin([intvl.sup for intvl in intvls])
+        # pop this interval
+        intvls[update_idx] = beds[update_idx].next()
+        # check that we're not at the end of the file
+        if intvls[update_idx] is None:
+            break
+        # recalculate column of sim_mat (calc once, ensure sim_mat is symmetric)
+        for i in range(update_idx):
+            sim_mat[i, update_idx] = similarity(intvls[i], intvls[update_idx])
+            sim_mat[update_idx, i] = sim_mat[i, update_idx]
+        for i in range(update_idx + 1, n):
+            sim_mat[i, update_idx] = similarity(intvls[i], intvls[update_idx])
+            sim_mat[update_idx, i] = sim_mat[i, update_idx]
+        # update minsim for (update_idx)-th sample
+        minsim[update_idx]["idx"] = np.argmin(sim_mat[:, update_idx])
+        minsim[update_idx]["s"] = sim_mat[update_idx, minsim[update_idx]["idx"]]
+        # update minsim for any sample where minsim[j]["idx"] == update_idx
+        for i in [i for i, ms in enumerate(minsim) if ms["idx"] == update_idx]:
+            minsim[i]["idx"] = np.argmin(sim_mat[:, i])
+            minsim[i]["s"] = sim_mat[i, minsim[i]["idx"]]
+    # save records as a DataFrame
+    df = pd.DataFrame(records, columns=column_names)
+    # save to output
+    df.to_csv(prefix + ".tsv", index=False, sep="\t")
+    return df
